9df8c7587c0f5b3eb9d2fbffadfce65b912a1b98
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-11-12 17:22:05 +0100

    refactor(weather): replace magic numbers with enum in getWeatherIcons switch

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -25,35 +25,43 @@ export const getWeatherContainers = (): WeatherContainers => {
 
 export const getWeatherIcons = (code: number, is_day: number): { src: string, alt: string } => {
     
+    enum weatherCode {
+        clear = 0,
+        partialCloudy = 2,
+        cloudy = 48,
+        rain = 82,
+        snow = 86,
+        thunderstorm = 99
+    }
     const path = "./images/weather_icons/";
 
     switch (true) {
-        case code === 0:
+        case code === weatherCode.clear:
             return { 
                 src: is_day ? `${path}day.svg` : `${path}night.svg`,
                 alt: is_day ? "Clear sky during the day" : "Clear sky at night"
             };
-        case code <= 2:
+        case code <= weatherCode.partialCloudy :
             return { 
                 src: is_day ? `${path}cloudy-day.svg` : `${path}cloudy-night.svg`,
                 alt: is_day ? "Partial cloudy day" : "Partial cloudy night"
             };
-        case code <= 48:
+        case code <= weatherCode.cloudy :
             return { 
                 src: `${path}cloudy.svg`,
                 alt: "Cloudy sky"
             };
-        case code <= 82:
+        case code <= weatherCode.rain :
             return { 
                 src: is_day ? `${path}rainy-day.svg` : `${path}rainy-night.svg`,
                 alt: is_day ? "Rainy day" : "Rainy night"
             };
-        case code <= 86:
+        case code <= weatherCode.snow :
             return { 
                 src: is_day ? `${path}snowy-day.svg` : `${path}snowy-night.svg`,
                 alt: is_day ? "Snowy day" : "Snowy night"
             };
-        case code <= 99:
+        case code <= weatherCode.thunderstorm :
             return { 
                 src: `${path}thunder.svg`,
                 alt: "Thunderstorm day"
